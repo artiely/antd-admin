a2c356e6a071fac61d671d24a39891f85455bc42
Artiely <1119696785@qq.com>
2019-01-03 19:33:37 +0800

    webhook

--- server.js
+++ server.js
@@ -5,18 +5,6 @@ var handler = createHandler({ path: '/incoming', secret: 'myHashSecret' })
 var shell = require('shelljs')
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
-handler.on('error', function(err) {
-  console.error('Error:', err.message)
-})
-
-handler.on('push', function(event) {
-  console.log(
-    'Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref
-  )
-  shell.exec('git fetch --all')
-})
 
 app.use(express.static(__dirname + '/dist'))
 // var proxy = require('http-proxy-middleware')
@@ -30,3 +18,16 @@ app.use(express.static(__dirname + '/dist'))
 // var exampleProxy = proxy(options) //开启代理功能，并加载配置
 // app.use('/ns-index', exampleProxy) //对地址为’/‘的请求全部转发
 app.listen(9999)
+
+handler.on('error', function(err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function(event) {
+  console.log(
+    'Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref
+  )
+  shell.exec('git fetch --all')
+})
