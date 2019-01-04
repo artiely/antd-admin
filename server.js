515b680ab44432119ab45947ce73bb3a34ae0afc
Artiely <1119696785@qq.com>
2019-01-04 09:41:37 +0800

    webhook

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var express = require('express')
 var app = express()
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/incoming', secret: 'myHashSecret' })
-var shell = require('shelljs')
+// var createHandler = require('github-webhook-handler')
+// var handler = createHandler({ path: '/incoming', secret: 'myHashSecret' })
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+var webhook = require('./webhook.js')
+// var bodyParser = require('body-parser')
+// var shell = require('shelljs')
+
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 
 app.use(express.static(__dirname + '/dist'))
+app.post('/webhook', webhook);
+
+
 // var proxy = require('http-proxy-middleware')
 // var options = {
 //   target: 'http://192.168.2.243:7070', // 测试
@@ -18,16 +26,3 @@ app.use(express.static(__dirname + '/dist'))
 // var exampleProxy = proxy(options) //开启代理功能，并加载配置
 // app.use('/ns-index', exampleProxy) //对地址为’/‘的请求全部转发
 app.listen(9999)
-
-handler.on('error', function(err) {
-  console.error('Error:', err.message)
-})
-
-handler.on('push', function(event) {
-  console.log(
-    'Received a push event for %s to %s',
-    event.payload.repository.name,
-    event.payload.ref
-  )
-  shell.exec('git fetch --all')
-})
---
+++ webhook.js
@@ -0,0 +1,30 @@
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: "myHashSecret" });
+var shell = require('shelljs')
+
+module.exports = function(req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  });
+
+  handler.on('error', function (err) {
+    console.error('Error:', err.message);
+  });
+
+  handler.on('push', function (event) {
+    shell.exec('git fetch --all')
+    //  var comps = event.payload.ref.split('/');
+    //  if (comps[2] != 'production') {
+    //    console.log('Received a push on %s and no build has is triggered', comps[2]);
+    //    return;
+    //  }
+    //  console.log('Received a push on production, build started...');
+    //  exec('./scripts/deploy', function(error, stdout, stderr) {
+    //    console.log(stdout);
+    //    if (error != null) {
+    //      console.log('Error during the execution of redeploy: ' + stderr);
+    //    }
+    //  });
+   });
+};
