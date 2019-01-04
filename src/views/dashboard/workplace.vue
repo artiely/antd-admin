92fe9b2c89a786b30ce5e9ef2b955d8f8e6d65bc
Artiely <1119696785@qq.com>
2019-01-04 14:21:02 +0800

    sj

--- src/views/dashboard/workplace.vue
+++ src/views/dashboard/workplace.vue
@@ -2,7 +2,7 @@
   <a-card>
     工作台 {{$t('message.hello')}} test auto deploy
     <a-time-picker use12Hours @change="onChange"/>
-    测试自动发布 123123 tanjie Artiely 888888888888  123
+    测试自动发布 123123 tanjie Artiely 888888888888  123 2019年1月4日14:20:43
   </a-card>
 </template>
 
