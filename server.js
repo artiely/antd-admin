fcd10494104f6b0c5c58613e1a66aab7d05d04a8
Artiely <1119696785@qq.com>
2019-01-23 11:10:10 +0800

    打包

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ webhookHandler.on('error', function(err, req, res) {
 // }
 // var exampleProxy = proxy(options) //开启代理功能，并加载配置
 // app.use('/ns-index', exampleProxy) //对地址为’/‘的请求全部转发
-app.listen(9999)
+app.listen(8888)
