07e6f54608cb33441f375969cb079d8318cda4cf
Artiely <1119696785@qq.com>
2019-01-25 14:51:22 +0800

    锁屏及路由控制

--- src/router/index.js
+++ src/router/index.js
@@ -18,28 +18,50 @@ const router = new Router({
 })
 
 // 处理登录
-console.log('123', store)
 router.beforeEach((to, from, next) => {
   NProgress.start()
+  let token = Cookies.get('token')
   if (to.meta.auth) {
-    let token = Cookies.get('token')
+    console.log(`需要鉴权`)
     let isLock = store.state.sys.isLock
     if (token) {
+      console.log(`需要鉴权：已登录`)
       if (isLock) {
-        next({ path: '/lock' })
+        console.log(`需要鉴权：已登录：并锁屏`)
+        next(false)
+        NProgress.done()
       } else {
+        console.log(`需要鉴权：已登录：未锁屏`)
         next()
       }
     } else {
+      console.log(`需要鉴权：未登陆 转登录页`)
       next({
         path: '/login',
         query: {
           redirect: to.fullPath,
         },
       })
+      NProgress.done()
     }
   } else {
-    next()
+    console.log(`不需要鉴权`)
+    if (token) {
+      next()
+      console.log(`不需要鉴权：登陆过有token`)
+    } else {
+      console.log(`不需要鉴权：没登陆过`)
+      if (to.path === '/login') {
+        console.log(`不需要鉴权：没登陆过：去登录页`)
+        next()
+      } else {
+        console.log(`不需要鉴权：没登陆过：去其他未知页 转登录页`)
+        next({
+          path: '/login',
+        })
+        NProgress.done()
+      }
+    }
   }
 })
 router.afterEach(() => {
